1c0d3146e444047ac45dedcc30827af0d707fda4
Coooolbreeze <392113643@qq.com>
2020-07-12 08:08:03 +0000

    httpserver test

--- container/nonrepeating.go
+++ container/nonrepeating.go
@@ -1,6 +1,27 @@
 package main
 
 func lengthOfNonRepeatingSubStr(s string) int {
+	lastOccurred := make([]int, 0xffff)
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
+	start := 0
+	maxLength := 0
+
+	for i, ch := range []rune(s) {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+
+	return maxLength
+}
+
+func lengthOfNonRepeatingSubStrCopy(s string) int {
 	lastOccurred := make(map[rune]int)
 	start := 0
 	maxLength := 0
